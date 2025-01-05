acd3dc62fb8db3176efeee929118cd4e906acfa4
eremhia <eremhia.tech@gmail.com>
2025-01-06 05:40:36 +0900

    setbill

---
+++ commands/bill/set-billing.js
@@ -0,0 +1,81 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('set-bill')
+        .setDescription('定期的な支払いを登録します')
+        .addStringOption(option =>
+            option.setName('name')
+                .setDescription('支払い名')
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('amount')
+                .setDescription('金額(円)')
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('month')
+                .setDescription('支払い月')
+                .setRequired(true)
+                .setMinValue(1)
+                .setMaxValue(12))
+
+        .addIntegerOption(option =>
+            option.setName('interval')
+                .setDescription('支払い間隔(月)')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('note')
+                .setDescription('メモ')
+                .setRequired(false)),
+    async execute(interaction) {
+
+        const name = interaction.options.getString('name');
+        const note = interaction.options.getString('note');
+        const amount = interaction.options.getInteger('amount');
+        const month = interaction.options.getInteger('month');
+        const interval = interaction.options.getInteger('interval');
+
+        const message = `支払い名: ${name}\n金額: ${amount}円\n支払い月: ${month}月\n支払い間隔: ${interval}月ごと\nメモ: ${note}\n\n登録しますか？`;
+
+        const confirm = new ButtonBuilder()
+            .setCustomId('confirm')
+            .setLabel('登録する')
+            .setStyle(ButtonStyle.Success);
+
+        const cancel = new ButtonBuilder()
+            .setCustomId('cancel')
+            .setLabel('キャンセル')
+            .setStyle(ButtonStyle.Danger);
+
+        const row = new ActionRowBuilder()
+            .addComponents(cancel, confirm);
+        const response = await interaction.reply({
+            content: message,
+            components: [row],
+        });
+        const collectorFilter = i => i.user.id === interaction.user.id;
+
+        try {
+            const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 120_000 });
+            if (confirmation.customId === 'confirm') {
+                //登録処理
+                const path = `./bills/${interaction.user.id}`;
+                if (!fs.existsSync(path)) {
+                    fs.mkdirSync(path);
+                }
+                fs.writeFileSync(`./bills/${interaction.user.id}/${interaction.id}.json`, JSON.stringify({
+                    name: name,
+                    amount: amount,
+                    month: month,
+                    interval: interval,
+                    note: note
+                }));
+                await confirmation.update({ content: `定期的な支払いを登録しました\n支払いの月にリマインダーを送信します`, components: [] });
+            } else if (confirmation.customId === 'cancel') {
+                await confirmation.update({ content: 'Action cancelled', components: [] });
+            }
+        } catch (e) {
+            await interaction.editReply({ content: '2分以内に応答がなかったため、登録をキャンセルしました', components: [] });
+        }
+    }
+};
