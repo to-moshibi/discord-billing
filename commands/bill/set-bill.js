51b9033c23461aa7cc109feaeb3df9732c9fbd06
eremhia <eremhia.tech@gmail.com>
2025-01-06 11:01:05 +0900

    Dfix

--- commands/bill/set-bill.js
+++ commands/bill/set-bill.js
@@ -37,7 +37,7 @@ module.exports = {
         const amount = interaction.options.getInteger('amount');
         const month = interaction.options.getInteger('month');
         const interval = interaction.options.getInteger('interval');
-        const message = `支払い名: ${name}\n金額: ${amount}円\n支払い月: ${month}月\n支払い間隔: ${interval}か月ごと\n次回支払い: ${(interval+interval)%12}月\nメモ: ${note}\n\n登録しますか？`;
+        const message = `支払い名: ${name}\n金額: ${amount}円\n支払い月: ${month}月\n支払い間隔: ${interval}か月ごと\n次回支払い: ${(month+interval)%12}月\nメモ: ${note}\n\n登録しますか？`;
 
         const confirm = new ButtonBuilder()
             .setCustomId('confirm')
