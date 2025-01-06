2d5e2ceb9b02215bde0b592dfa06566946237961
eremhia <eremhia.tech@gmail.com>
2025-01-06 21:20:13 +0900

    dix

--- commands/utility/help-bill.js
+++ commands/utility/help-bill.js
@@ -13,7 +13,7 @@ module.exports = {
             .setTitle('支払いに関するコマンドのヘルプ')
             .setDescription('支払いに関するコマンドのヘルプです。\n支払いを変更する場合は、一度消去して下さい')
             .addFields(
-                { name: '/add-bill', value: '支払いを登録します。' },
+                { name: '/set-bill', value: '支払いを登録します。' },
                 { name: '/ls-bill', value: '登録されている支払いを表示します。' },
                 { name: '/rm-bill', value: '登録されている支払いを削除します。' },
                 { name: '/pay-bill', value: '登録されている支払いを支払い済みにします。' }
